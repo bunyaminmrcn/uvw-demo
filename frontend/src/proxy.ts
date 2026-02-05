2e9188dc49dfa7cb51858b576e86535242ceaa1d
bunyaminmrcn <bunyaminmrcn@gmail.com>
2026-02-05 14:56:14 +0300

    proxy

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import Cookie from 'js-cookie'
 
-export function middleware(request: NextRequest) {
+export default function middleware(request: NextRequest) {
 
   const token = request.cookies.get('token')?.value
 
