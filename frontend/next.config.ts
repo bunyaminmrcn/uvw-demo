044433985d5ba35864ae66fc3850a5e3be4801b4
bunyaminmrcn <bunyaminmrcn@gmail.com>
2026-02-05 13:12:25 +0300

    update front

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Only run on client-side builds
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
