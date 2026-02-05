0dc5bb079e43000663e324945e5927463d99f038
bunyaminmrcn <bunyaminmrcn@gmail.com>
2026-02-05 14:15:07 +0300

    webpack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  webpack: (config, { isServer }) => {
-    // Only run on client-side builds
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
