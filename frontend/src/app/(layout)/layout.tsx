9c2301f7d136fac0a63ed0440b69c4bc3210c057
bunyaminmrcn <bunyaminmrcn@gmail.com>
2026-02-05 13:35:44 +0300

    webpack

--- frontend/src/app/(layout)/layout.tsx
+++ frontend/src/app/(layout)/layout.tsx
@@ -1,8 +1,8 @@
 
 import Auth from "./_auth"
+import { ReactNode } from "react"
 
 
-
-export default function App() {
-    return <Auth />
+export default function App({children}: { children: ReactNode }) {
+    return <Auth>{children}</Auth>
 }
